462151f82f3a14e315ed29a6f12c7595bf16ce4e
Zonetto <as076053@gmail.com>
2024-11-21 22:04:02 +0300

    UPD: Simplified Project Cards
    - Replaced `button` elements with `a` tags styled as buttons for better semantic usage and direct linking.
    - Adjusted the `projects-section` and `projects-cards` layout for improved styling consistency.
    - Retained API fetch logic and data structure transformations.

--- src/js/projects_component.js
+++ src/js/projects_component.js
@@ -1,40 +1,32 @@
 
 Vue.createApp({
   template: `
-    <section id="projects" class="projects-section">
-        <h1>My <span>Projects</span></h1>
-      <div class="projects-cards">
+  <div class="projects-section">
+    <h1>My <span>Projects</span></h1>
+    <div class="projects-cards">
         <div class="projects-card" v-for="(card, index) in cards" :key="index">
-          <img :src="card.image" :alt="card.title">
-          <div class="projects-info">
-            <h1>{{ card.title }}</h1>
-            <div class="projects-made">
-              <h4>Made with:</h4>
-              <iconify-icon 
-                v-for="(icon, iconIndex) in card.icons" 
-                :key="iconIndex" 
-                :icon="icon"
-                width="25" 
-                height="25">
-              </iconify-icon>
+            <img :src="card.image" :alt="card.title">
+            <div class="projects-info">
+                <h1>{{ card.title }}</h1>
+                <div class="projects-made">
+                    <h4>Made with:</h4>
+                    <iconify-icon v-for="(icon, iconIndex) in card.icons" :key="iconIndex" :icon="icon" width="25"
+                        height="25">
+                    </iconify-icon>
+                </div>
+                <h4>{{ card.description }}</h4>
+                <a v-for="(button, btnIndex) in card.buttons" :key="btnIndex" class="btn-blue"
+                    @click="openLink(button.link)">
+                    <h5>{{ button.text }}</h5>
+                </a>
             </div>
-            <h4>{{ card.description }}</h4>
-            <div class="projects-btns">
-              <button v-for="(button, btnIndex) in card.buttons" 
-                      :key="btnIndex" 
-                      @click="openLink(button.link)">
-                {{ button.text }}
-              </button>
-            </div>
-          </div>
         </div>
-      </div>
-    </section>
-  `,
+    </div>
+</div>
+`,
   data() {
     return {
-      cards: [
-      ]
+      cards: [],
     }
   },
   methods: {
