60b03e04852f6458c6789ac71f90d52d3914a68e
Zonetto <as076053@gmail.com>
2024-11-22 10:18:11 +0300

    refactor: Enhance ContactComponent with dynamic API data fetching
    - Improved `fetchContacts` method to dynamically fetch contact details from an API.
    - Retained the same functionality for rendering contact cards with updated API integration.
    - Simplified and ensured consistent error handling for data fetching.

--- src/js/contact_component.js
+++ src/js/contact_component.js
@@ -17,33 +17,42 @@ Vue.createApp({
     `,
     data() {
         return {
-            contacts: [
-                {
-                    icon: "mdi:linkedin",
-                    link: "https://linkedin.com/in/1ahmedsaad",
-                    username: "1ahmedsaad"
-                },
-                {
-                    icon: "logos:telegram",
-                    link: "https://t.me/ahmed4saad",
-                    username: "@ahmed4saad"
-                },
-                {
-                    icon: "mdi:email",
-                    link: "mailto:ahmed.saad.shuker@gmail.com",
-                    username: "ahmed.saad.shuker"
-                },
-                {
-                    icon: "radix-icons:github-logo",
-                    link: "https://github.com/Zonetto",
-                    username: "Zonetto"
-                }
-            ]
+            contacts: []
         };
     },
     methods: {
         openLink(link) {
             window.open(link, "_blank");
+        },
+        async fetchContacts() {
+            const apiUrl = "https://zonetto.github.io/portfolio-api/api.json";
+            const requestOptions = {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                redirect: "follow"
+            };
+
+            try {
+                const response = await fetch(apiUrl, requestOptions);
+                const data = await response.json();
+
+                // Transform fetched data into the desired card structure
+                this.contacts = data.contacts.map(item => ({
+                    username: item.username || "No Username",
+                    link: item.link || "No Link",
+                    image: item.image || "https://images4.alphacoders.com/115/thumb-1920-115716.jpg",
+                    icon: item.icon || "mdi:github",
+                }));
+
+            } catch (error) {
+                console.error("Error fetching projects:", error);
+            }
         }
+
+    },
+    mounted() {
+        this.fetchContacts();
     }
 }).mount("#ContactComponent");
